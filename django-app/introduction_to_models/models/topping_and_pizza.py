d800110662f675f473b117512452c20d494d01ee
JeongEuiJin <hehar1020@gmail.com>
2017-06-06 23:13:59 +0900

    m2m m2o 이해는 되지만 코드로 짜는게 아직 어렵다

--- django-app/introduction_to_models/models/topping_and_pizza.py
+++ django-app/introduction_to_models/models/topping_and_pizza.py
@@ -16,7 +16,21 @@ class Pizza(models.Model):
     toppings = models.ManyToManyField(Topping)
 
     def __str__(self):
-        return self.name
+        # topping_string = ''
+        #
+        # for topping in self.toppings.all():
+        #     topping_string += topping.name
+        #     topping_string += ', '
+        # topping_string = topping_string[:-2]
+        # return '{} ({})'.format(
+        #     self.name,
+        #     topping_string
+        # )
+        return '{} ({})'.format(self.name,', '.join([t.name for t in self.toppings()]))
+
+        # 자신이 가지고있는 토핑목록을 뒤에 출력
+        # ex) 치즈피자 (치즈, 토마스)
+        # return self.name
 
     class Meta:
         ordering = ('name',)
