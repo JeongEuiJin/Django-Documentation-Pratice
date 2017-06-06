8661a8b5e465d596daaf28ed08befe5853545171
JeongEuiJin <hehar1020@gmail.com>
2017-06-06 13:45:13 +0900

    person model modify(choices, reqursive foreign key add) car, manufacturer add

--- django-app/introduction_to_models/models.py
+++ django-app/introduction_to_models/models.py
@@ -7,8 +7,45 @@ class Person(models.Model):
         ('M', 'Medium'),
         ('L', 'Large'),
     )
+    PERSON_TYPES = (
+        ('studnet','학생'),
+        ('teacher', '선생'),
+    )
+    person_type = models.CharField(
+        '유형',
+        max_length=10,
+        choices=PERSON_TYPES,
+        default=PERSON_TYPES[0][0]
+    )
+    teacher = models.ForeignKey(
+        'self',
+        null=True,
+        on_delete=models.CASCADE,
+        blank=True,
+    )
     name = models.CharField(max_length=60)
     shirt_size = models.CharField(
         max_length=1,
-        choices=SHIRT_SIZES
+        choices=SHIRT_SIZES,
+        help_text='남자는 L을 쓰세요',
     )
+    def __str__(self):
+        return self.name
+
+
+
+class ManuFacturer(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    name = models.CharField(max_length=40)
+    manufacturer = models.ForeignKey(
+        ManuFacturer,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
