659ec0e7e76f285f00612aef05afbd07bcb5601c
JeongEuiJin <hehar1020@gmail.com>
2017-06-07 11:52:07 +0900

    hw code add

--- django-app/introduction_to_models/models/intermediate.py
+++ django-app/introduction_to_models/models/intermediate.py
@@ -7,6 +7,14 @@ class Player(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def current_club(self):
+        return self.current_tradinfo.club
+
+    @property
+    def current_tradinfo(self):
+        return self.tradinfo_set.get(date_leaved__isnull=True)
+
         # current_club프로퍼티에 현재 속하는 Club리턴
         # current_tradeinfo프로퍼티에 현재 자신의 TradeInfo리턴
 
@@ -16,13 +24,23 @@ class Club(models.Model):
     players = models.ManyToManyField(
         Player,
         through='TradInFo',
+        through_fields=('club','player'),
     )
 
     def __str__(self):
         return self.name
 
     def squad(self, year=None):
-        pass
+        if year:
+            return self.players.filter(
+                tradinfo__date_joined__lte=date(year+1,1,1),
+                tradinfo__date_leaved__gt=date(year,1,1),
+            )
+
+
+        else:
+            return self.players.filter(tradinfo__date_leaved__isnull=True)
+            pass
         # squad메서드에 현직 선수들만 리턴
         # 인수로 년도(2017, 2015...등)를 받아
         # 해당 년도의 현직 선수들을 리턴,
@@ -34,6 +52,36 @@ class TradInFo(models.Model):
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
     date_joined = models.DateField()
     date_leaved = models.DateField(null=True, blank=True)
+    recommender = models.ForeignKey(
+        Player,
+        on_delete=models.PROTECT,
+        related_name='tradinfo_set_by_recommender',
+        null=True,
+        blank=True,
+    )
+
+    prev_club = models.ForeignKey(
+        Club,
+        on_delete=models.PROTECT,
+        related_name='+',
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+
+        return '{}, {} ({} ~{})'.format(
+            self.player.name,
+            self.club.name,
+            self.date_joined,
+            self.date_leaved or '현직'
+            # self.date_leaved if self.date_leaved else '현직'
+
+        )
+    @property
+    def is_current(self):
+        return self.date_leaved is None
+        # return not self.date_leaved
 
 
 
