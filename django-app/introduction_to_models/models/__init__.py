66384f2aa37c96c19c99cba89e7f270e59dded8d
JeongEuiJin <hehar1020@gmail.com>
2017-06-07 15:40:53 +0900

    abc 실습

--- django-app/introduction_to_models/models/__init__.py
+++ django-app/introduction_to_models/models/__init__.py
@@ -4,3 +4,5 @@ from .many_to_one_examply import Report, Article
 from .topping_and_pizza import Topping, Pizza
 from .intermediate import Player, Club, TradInFo
 from .one_to_one_examply import Restaurant,Waiter,Place
+from .abstract_base_classes import Student,Teacher, CommonInfo
+from .multi_table_inheritance import CommonInfo2, Student2, Teacher2
